782c308cc6c7fcd3ebbf5e88e392ba734bfa9c6e
Matias Iturbides <iturbides@keemail.me>
2025-06-28 14:15:28 +0200

    First commit

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "hora-solar-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+// Instalar
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Activar
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+// Interceptar peticiones
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request);
+    })
+  );
+});
+
