522e74860f60d7dec0b1b61b9945126f206b771f
Matias Iturbides <iturbides@keemail.me>
2025-06-30 08:44:16 +0200

    quité server worker y manifest para ver sin funciona mejor en vivaldi

--- main.js
+++ main.js
@@ -153,12 +153,4 @@
       }
     };
 
-// Registro del Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(reg => console.log('✅ Service Worker registrado:', reg.scope))
-      .catch(err => console.error('❌ Error al registrar el Service Worker:', err));
-  });
-}
 
